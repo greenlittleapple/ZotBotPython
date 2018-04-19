e3baf646e4617e94ed2103e751d2c09a88edd537
Marcus <greenlittleapple@gmail.com>
2018-04-18 18:09:38 -0700

    trying to fix stuck processing, pt 4 of infinity

--- reddit_handler.py
+++ reddit_handler.py
@@ -26,11 +26,7 @@ async def retrieve_and_post_UCI():
         global uci_running
         uci_running = True
         botutils.write_to_log('Checking for new posts...')
-        processed = 0
         for submission in sorted(uci_sub.new(limit=20), key=lambda x: x.created_utc):
-            processed += 1
-            if processed == 50:  # Stop giant for loop, for some reason it gets stuck?
-                break
             file = open('uci_posts.txt', 'r+')
             if not str(submission.id + "\n") in file.readlines():
                 embed = discord.Embed(
@@ -70,14 +66,14 @@ async def retrieve_and_post_UCI():
                 embed.add_field(name='Topics', value=' '.join([('**「' + x + '」**') for x in categories]), inline=False)
                 try:
                     botutils.write_to_log('Trying to post...')
-                    await asyncio.wait_for(head.client.send_message(botutils.get_chan_by_id("421828948159365120"), embed=embed), timeout=5)
+                    fut = head.client.send_message(botutils.get_chan_by_id("421828948159365120"), embed=embed)
+                    await asyncio.wait_for(fut, timeout=5)
                     botutils.write_to_log('Submission Posted: ' + submission.title)
-                except Exception as e:
-                    botutils.write_to_log('ERROR: ' + str(e) + '\n' + 'Error in Submission: ' + submission.title)
-                finally:
                     with open('uci_posts.txt', 'a') as write_file:
                         write_file.write(submission.id + "\n")
                         write_file.flush()
+                except Exception as e:
+                    botutils.write_to_log('ERROR: ' + str(e) + '\n' + 'Error in Submission: ' + submission.title)
         file.close()
         uci_running = False
         await asyncio.sleep(60)
